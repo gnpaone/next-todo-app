57b52307a9c49ae057beddba1c6034b0387e1603
Naveen Prashanth <78990165+gnpaone@users.noreply.github.com>
2024-07-26 13:05:34 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: "standalone",
   	basePath: process.env.BASE_PATH ? process.env.BASE_PATH : "",
 	reactStrictMode: false,
 };
