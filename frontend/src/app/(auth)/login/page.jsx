962248d10ed60c49bdd26f83d15d725a6f5c4b54
Naveen Prashanth <78990165+gnpaone@users.noreply.github.com>
2024-07-26 14:27:30 +0530

    Update page.jsx

--- frontend/src/app/(auth)/login/page.jsx
+++ frontend/src/app/(auth)/login/page.jsx
@@ -52,7 +52,7 @@ const Login = () => {
 				createCookie(data.data.token, data.data.user);
 				setLoading(false);
 				router.push("/");
-				router.refresh()
+				router.refresh();
 				if (typeof window === 'undefined') {
 			        revalidatePath("/");
 			    }
